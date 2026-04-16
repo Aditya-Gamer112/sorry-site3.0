0bebce98dfc35a75ff9275b053d7a3e50a477ad4
Aditya-Gamer112 <adityakumar110071@gmail.com>
2026-04-16 18:34:26 +0530

    Update ApologyPage.jsx

--- src/components/ApologyPage.jsx
+++ src/components/ApologyPage.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react"
 export default function ApologyPage({ setCurrentPage }) {
   const [currentText, setCurrentText] = useState(0)
 
-  const texts = ["I know I hurt you...", "You didn't deserve that...", "I was wrong, and I'm so sorry..."]
+  const texts = ["I know I hurt you...", "You didn't deserve that Sammy...", "I anony you so much, and I'm so sorry..."]
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -64,7 +64,7 @@ export default function ApologyPage({ setCurrentPage }) {
         animate={{ opacity: 1 }}
         transition={{ delay: 2 }}
       >
-        Read my letter...
+        Read my letter Sammy...
       </motion.button>
     </div>
   )
